9d034d042ba6ac33270e4dcf9893fc70b04a13c9
Ed Roberts Rayne <ed.rayne@gmail.com>
2022-12-11 15:05:39 +0000

    Update Kalman.h
    All F(" ") strings removed so it compiles on Nano 33 BLE Sense.

--- Kalman.h
+++ Kalman.h
@@ -26,6 +26,8 @@
  * Author:
  *  R.JL. Fétick
  *
+ * Modified by:
+ *  Ed Roberts Rayne
  */
 
 #ifndef Kalman_h
@@ -45,194 +47,248 @@ using namespace BLA;
 // If your matrices are symmetric or triangular, use these ones for Arduino SRAM saving
 
 // Symmetric (i=row*cols+col)
-template<int dim, class ElemT> struct Symmetric{
-  mutable ElemT m[dim*(dim+1)/2];
+template <int dim, class ElemT>
+struct Symmetric
+{
+  mutable ElemT m[dim * (dim + 1) / 2];
   typedef ElemT elem_t;
-  ElemT &operator()(int row, int col) const{
+  ElemT &operator()(int row, int col) const
+  {
     static ElemT dummy;
-    if(col < dim && row < dim){
-      if(col < row){ // swap row and col
+    if (col < dim && row < dim)
+    {
+      if (col < row)
+      { // swap row and col
         ElemT temp = row;
         row = col;
         col = temp;
       }
-      return m[(2*dim-row+1)*row/2+col-row];
-    }else
+      return m[(2 * dim - row + 1) * row / 2 + col - row];
+    }
+    else
       return (dummy = 0);
   }
 };
 
 // SkewSymmetric (i=row*cols+col)
-template<int dim, class ElemT> struct SkewSymmetric{
-  mutable ElemT m[dim*(dim+1)/2];
+template <int dim, class ElemT>
+struct SkewSymmetric
+{
+  mutable ElemT m[dim * (dim + 1) / 2];
   typedef ElemT elem_t;
-  ElemT &operator()(int row, int col) const{
+  ElemT &operator()(int row, int col) const
+  {
     static ElemT dummy;
-    if(col < dim && row < dim && row!=col){
-		if(col < row){
-			return -m[(2*dim-col+1)*col/2+row-col];
-		}else{
-			return m[(2*dim-row+1)*row/2+col-row];
-		}
-    }else{
+    if (col < dim && row < dim && row != col)
+    {
+      if (col < row)
+      {
+        return -m[(2 * dim - col + 1) * col / 2 + row - col];
+      }
+      else
+      {
+        return m[(2 * dim - row + 1) * row / 2 + col - row];
+      }
+    }
+    else
+    {
       return (dummy = 0);
-	}
+    }
   }
 };
 
 // Triangular sup (i=row*cols+col)
-template<int dim, class ElemT> struct TriangularSup{
-  mutable ElemT m[dim*(dim+1)/2];
+template <int dim, class ElemT>
+struct TriangularSup
+{
+  mutable ElemT m[dim * (dim + 1) / 2];
   typedef ElemT elem_t;
-  ElemT &operator()(int row, int col) const{
+  ElemT &operator()(int row, int col) const
+  {
     static ElemT dummy;
-    if(col < dim && row < dim && col>=row){
-      return m[(2*dim-row+1)*row/2+col-row];
-    }else
+    if (col < dim && row < dim && col >= row)
+    {
+      return m[(2 * dim - row + 1) * row / 2 + col - row];
+    }
+    else
       return (dummy = 0);
   }
 };
 
 // Triangular inf (i=row*cols+col)
-template<int dim, class ElemT> struct TriangularInf{
-  mutable ElemT m[dim*(dim+1)/2];
+template <int dim, class ElemT>
+struct TriangularInf
+{
+  mutable ElemT m[dim * (dim + 1) / 2];
   typedef ElemT elem_t;
-  ElemT &operator()(int row, int col) const{
+  ElemT &operator()(int row, int col) const
+  {
     static ElemT dummy;
-    if(col < dim && row < dim && row>=col){
-      return m[(2*dim-col+1)*col/2+row-col];
-    }else
+    if (col < dim && row < dim && row >= col)
+    {
+      return m[(2 * dim - col + 1) * col / 2 + row - col];
+    }
+    else
       return (dummy = 0);
   }
 };
 
 // Diagonal template comes from Tom Stewart (BasicLinearAlgebra)
-template<int dim, class ElemT> struct Diagonal{
-    mutable ElemT m[dim];
-    // The only requirement on this class is that it implement the () operator like so:
-    typedef ElemT elem_t;
+template <int dim, class ElemT>
+struct Diagonal
+{
+  mutable ElemT m[dim];
+  // The only requirement on this class is that it implement the () operator like so:
+  typedef ElemT elem_t;
 
-    ElemT &operator()(int row, int col) const
-    {
-        static ElemT dummy;
-        // If it's on the diagonal and it's not larger than the matrix dimensions then return the element
-        if(row == col && row < dim)
-            return m[row];
-        else
-            return (dummy = 0);
-    }
+  ElemT &operator()(int row, int col) const
+  {
+    static ElemT dummy;
+    // If it's on the diagonal and it's not larger than the matrix dimensions then return the element
+    if (row == col && row < dim)
+      return m[row];
+    else
+      return (dummy = 0);
+  }
 };
-
-
 
 /**********      CLASS DEFINITION      **********/
 
 // Last arg of template allows to eventually define Symmetric, AntiSymmetric, TriangularSup or TriangularInf matrices for memory saving
-template<int Nstate, int Nobs, int Ncom = 0, class MemF = Array<Nstate,Nstate,float> >
-class KALMAN{
-  private:
-    void _update(BLA::Matrix<Nobs> obs, BLA::Matrix<Nstate> comstate);
-    BLA::Identity<Nstate,Nstate> Id; // Identity matrix
-  public:
-	//INPUT MATRICES			
-    BLA::Matrix<Nstate,Nstate,MemF> F; // time evolution matrix
-    BLA::Matrix<Nobs,Nstate> H; // observation matrix
-    BLA::Matrix<Nstate,Ncom> B; // Command matrix (optional)
-    BLA::Matrix<Nstate,Nstate, Symmetric<Nstate,float> > Q; // model noise covariance matrix
-    BLA::Matrix<Nobs,Nobs, Symmetric<Nobs,float> > R; // measure noise covariance matrix
-	//OUTPUT MATRICES
-    BLA::Matrix<Nstate,Nstate, Symmetric<Nstate,float> > P; // posterior covariance (do not modify, except to init!)
-    BLA::Matrix<Nstate> x; // state vector (do not modify, except to init!)
+template <int Nstate, int Nobs, int Ncom = 0, class MemF = Array<Nstate, Nstate, float>>
+class KALMAN
+{
+private:
+  void _update(BLA::Matrix<Nobs> obs, BLA::Matrix<Nstate> comstate);
+  BLA::Identity<Nstate, Nstate> Id; // Identity matrix
+public:
+  // INPUT MATRICES
+  BLA::Matrix<Nstate, Nstate, MemF> F;                     // time evolution matrix
+  BLA::Matrix<Nobs, Nstate> H;                             // observation matrix
+  BLA::Matrix<Nstate, Ncom> B;                             // Command matrix (optional)
+  BLA::Matrix<Nstate, Nstate, Symmetric<Nstate, float>> Q; // model noise covariance matrix
+  BLA::Matrix<Nobs, Nobs, Symmetric<Nobs, float>> R;       // measure noise covariance matrix
+                                                           // OUTPUT MATRICES
+  BLA::Matrix<Nstate, Nstate, Symmetric<Nstate, float>> P; // posterior covariance (do not modify, except to init!)
+  BLA::Matrix<Nstate> x;                                   // state vector (do not modify, except to init!)
 
-    int status; // 0 if Kalman filter computed correctly
+  int status; // 0 if Kalman filter computed correctly
 
-    // UPDATE FILTER WITH OBSERVATION
-    void update(BLA::Matrix<Nobs> obs);
+  // UPDATE FILTER WITH OBSERVATION
+  void update(BLA::Matrix<Nobs> obs);
 
-    // UPDATE FILTER WITH OBSERVATION and COMMAND
-    void update(BLA::Matrix<Nobs> obs, BLA::Matrix<Ncom> com);
+  // UPDATE FILTER WITH OBSERVATION and COMMAND
+  void update(BLA::Matrix<Nobs> obs, BLA::Matrix<Ncom> com);
 
-    // CONSTRUCTOR
-  	KALMAN<Nstate,Nobs,Ncom,MemF>();
+  // CONSTRUCTOR
+  KALMAN<Nstate, Nobs, Ncom, MemF>();
 
-    // GETTER on X (copy vector to avoid eventual user modifications)
-    BLA::Matrix<Nstate> getxcopy();
-
+  // GETTER on X (copy vector to avoid eventual user modifications)
+  BLA::Matrix<Nstate> getxcopy();
 };
 
 /**********      PRIVATE IMPLEMENTATION of UPDATE      **********/
 
 template <int Nstate, int Nobs, int Ncom, class MemF>
-void KALMAN<Nstate,Nobs,Ncom,MemF>::_update(BLA::Matrix<Nobs> obs, BLA::Matrix<Nstate> comstate){
-  if(KALMAN_CHECK){
-    for(int i=0;i<Nobs;i++){
-      if(isnan(obs(i)) || isinf(obs(i))){
-        if(KALMAN_VERBOSE){Serial.println(F("KALMAN:ERROR: observation has nan or inf values"));}
+void KALMAN<Nstate, Nobs, Ncom, MemF>::_update(BLA::Matrix<Nobs> obs, BLA::Matrix<Nstate> comstate)
+{
+  if (KALMAN_CHECK)
+  {
+    for (int i = 0; i < Nobs; i++)
+    {
+      if (isnan(obs(i)) || isinf(obs(i)))
+      {
+        if (KALMAN_VERBOSE)
+        {
+          Serial.println("KALMAN:ERROR: observation has nan or inf values");
+        }
         status = 1;
         return;
       }
     }
   }
-  BLA::Matrix<Nobs,Nobs> S;
-  BLA::Matrix<Nstate,Nobs> K; // Kalman gain matrix
+  BLA::Matrix<Nobs, Nobs> S;
+  BLA::Matrix<Nstate, Nobs> K; // Kalman gain matrix
   // UPDATE
   this->x = this->F * this->x + comstate;
-  this->P = this->F * this->P * (~ this->F) + this->Q;
+  this->P = this->F * this->P * (~this->F) + this->Q;
   // ESTIMATION
-  S = this->H * this->P * (~ this->H) + this->R;
+  S = this->H * this->P * (~this->H) + this->R;
   bool is_nonsingular = Invert(S); // inverse inplace (S <- S^{-1})
-  K = P*(~H)*S;
-  if(is_nonsingular){
-    this->x += K*(obs - this->H * this->x); // K*y
-    this->P = (this->Id - K * this->H)* this->P;
-    if(KALMAN_CHECK){
-      for(int i=0;i<Nstate;i++){
-        if(isnan(this->x(i)) || isinf(this->x(i))){
-          if(KALMAN_VERBOSE){Serial.println(F("KALMAN:ERROR: estimated vector has nan or inf values"));}
+  K = P * (~H) * S;
+  if (is_nonsingular)
+  {
+    this->x += K * (obs - this->H * this->x); // K*y
+    this->P = (this->Id - K * this->H) * this->P;
+    if (KALMAN_CHECK)
+    {
+      for (int i = 0; i < Nstate; i++)
+      {
+        if (isnan(this->x(i)) || isinf(this->x(i)))
+        {
+          if (KALMAN_VERBOSE)
+          {
+            Serial.println("KALMAN:ERROR: estimated vector has nan or inf values");
+          }
           status = 1;
           return;
         }
       }
     }
-  }else{
-    if(KALMAN_VERBOSE){Serial.println(F("KALMAN:ERROR: could not invert S matrix. Try to reset P matrix."));}
+  }
+  else
+  {
+    if (KALMAN_VERBOSE)
+    {
+      Serial.println("KALMAN:ERROR: could not invert S matrix. Try to reset P matrix.");
+    }
     this->P.Fill(0.0); // try to reset P. Better strategy?
-    //K.Fill(0.0);
+    // K.Fill(0.0);
   }
 };
 
-
 /**********      UPDATE with OBS & COM      **********/
 template <int Nstate, int Nobs, int Ncom, class MemF>
-void KALMAN<Nstate,Nobs,Ncom,MemF>::update(BLA::Matrix<Nobs> obs, BLA::Matrix<Ncom> com){
-  if(KALMAN_CHECK){
-    for(int i=0;i<Ncom;i++){
-      if(isnan(com(i)) || isinf(com(i))){
-        if(KALMAN_VERBOSE){Serial.println(F("KALMAN:ERROR: command has nan or inf values"));}
+void KALMAN<Nstate, Nobs, Ncom, MemF>::update(BLA::Matrix<Nobs> obs, BLA::Matrix<Ncom> com)
+{
+  if (KALMAN_CHECK)
+  {
+    for (int i = 0; i < Ncom; i++)
+    {
+      if (isnan(com(i)) || isinf(com(i)))
+      {
+        if (KALMAN_VERBOSE)
+        {
+          Serial.println("KALMAN:ERROR: command has nan or inf values");
+        }
         status = 1;
         return;
       }
     }
   }
-  _update(obs,this->B *com);
+  _update(obs, this->B * com);
 };
 
 /**********      UPDATE with OBS      **********/
 
 template <int Nstate, int Nobs, int Ncom, class MemF>
-void KALMAN<Nstate,Nobs,Ncom,MemF>::update(BLA::Matrix<Nobs> obs){
-	BLA::Zeros<Nstate> NULLCOMSTATE;
-  _update(obs,NULLCOMSTATE);
+void KALMAN<Nstate, Nobs, Ncom, MemF>::update(BLA::Matrix<Nobs> obs)
+{
+  BLA::Zeros<Nstate> NULLCOMSTATE;
+  _update(obs, NULLCOMSTATE);
 };
 
 /**********      CONSTRUCTOR      **********/
 
 template <int Nstate, int Nobs, int Ncom, class MemF>
-KALMAN<Nstate,Nobs,Ncom,MemF>::KALMAN(){
-  if(KALMAN_VERBOSE){
-    Serial.println(F("KALMAN:INFO: Initialize filter"));
-    if((Nstate<=1)||(Nobs<=1)){
-      Serial.println(F("KALMAN:ERROR: 'Nstate' and 'Nobs' must be > 1"));
+KALMAN<Nstate, Nobs, Ncom, MemF>::KALMAN()
+{
+  if (KALMAN_VERBOSE)
+  {
+    // Serial.println(F("KALMAN:INFO: Initialize filter"));
+    if ((Nstate <= 1) || (Nobs <= 1))
+    {
+      Serial.println("KALMAN:ERROR: 'Nstate' and 'Nobs' must be > 1");
     }
   }
   this->P.Fill(0.0);
@@ -242,13 +298,14 @@ KALMAN<Nstate,Nobs,Ncom,MemF>::KALMAN(){
 /**********      GETXCOPY      **********/
 
 template <int Nstate, int Nobs, int Ncom, class MemF>
-BLA::Matrix<Nstate> KALMAN<Nstate,Nobs,Ncom,MemF>::getxcopy(){
+BLA::Matrix<Nstate> KALMAN<Nstate, Nobs, Ncom, MemF>::getxcopy()
+{
   BLA::Matrix<Nstate> out;
-  for(int i=0;i<Nstate;i++){
+  for (int i = 0; i < Nstate; i++)
+  {
     out(i) = this->x(i);
   }
   return out;
 };
-
 
 #endif
